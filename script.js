42951f1ac4ffc82ddc45d4dbbecaa1f350623f79
nasana3 <88834626+nasana3@users.noreply.github.com>
2025-10-29 14:37:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 const toggleBtn = document.getElementById('theme-toggle');
 const body = document.body;
 
-// Immer Darkmode aktivieren beim Laden
 body.classList.add('dark');
 toggleBtn.textContent = '☀️';
 
-// Optional: Umschalten erlauben
 toggleBtn.addEventListener('click', () => {
   body.classList.toggle('dark');
   const isDark = body.classList.contains('dark');
